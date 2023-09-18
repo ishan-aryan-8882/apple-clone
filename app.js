20f8fe44e1989511f2a10e395082507a3a8f9122
Ishan Aryan <toxic.valorant.yt@gmail.com>
2023-09-18 07:05:53 +0530

    made hero resizable

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ let lone = document.querySelector(".lone");
 let ltwo = document.querySelector(".ltwo");
 let nava = document.querySelectorAll("nav a");
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle("nav-appear");
-    lone.classList.toggle("lone-cross");
-    ltwo.classList.toggle("ltwo-cross");
-    nava.forEach((item, index) => {
-        setTimeout(() => {
-            item.classList.toggle('nav-appear');
-        }, index * 50);
-    });
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-appear");
+  lone.classList.toggle("lone-cross");
+  ltwo.classList.toggle("ltwo-cross");
+  nava.forEach((item, index) => {
+    setTimeout(() => {
+      item.classList.toggle("nav-appear");
+    }, index * 50);
+  });
 });
+
+let hero = document.querySelector(".hero");
+let image = document.querySelector(".hero img");
+let hw = image.clientHeight;
+
+console.log(hw);
